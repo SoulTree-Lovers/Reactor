0350020b72f255302863db91a49dc81df1e48434
kangseungmin <rkdtmdals999@naver.com>
2024-03-03 13:35:39 +0900

    [feat] Publisher의 Flux, Mono 메소드 추가

--- src/main/java/org/example/publishers/Publisher.java
+++ src/main/java/org/example/publishers/Publisher.java
@@ -3,11 +3,17 @@ package org.example.publishers;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 public class Publisher {
 
     // Flux: 여러 값
     public Flux<Integer> startFlux() {
         return Flux.range(1, 10).log();
+    }
+
+    public Flux<String> startFluxList() {
+        return Flux.fromIterable(List.of("a", "b", "c", "d")).log();
     }
 
     // Mono: 단일 값
@@ -18,4 +24,10 @@ public class Publisher {
     public Mono<?> startMonoEmpty() {
         return Mono.empty().log();
     }
+
+    public Mono<?> startMonoError() {
+        return Mono.error(new Exception("mono error"));
+    }
+
+
 }
