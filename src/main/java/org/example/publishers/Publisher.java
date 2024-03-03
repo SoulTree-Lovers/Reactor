46d5ee90cf720d873084d44d1abe95177426b078
kangseungmin <rkdtmdals999@naver.com>
2024-03-03 12:05:19 +0900

    [feat] Publisher Mono 메소드 구현

--- src/main/java/org/example/publishers/Publisher.java
+++ src/main/java/org/example/publishers/Publisher.java
@@ -1,10 +1,21 @@
 package org.example.publishers;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class Publisher {
 
+    // Flux: 여러 값
     public Flux<Integer> startFlux() {
-        return Flux.range(1, 10);
+        return Flux.range(1, 10).log();
+    }
+
+    // Mono: 단일 값
+    public Mono<Integer> startMono() {
+        return Mono.just(1).log();
+    }
+
+    public Mono<?> startMonoEmpty() {
+        return Mono.empty().log();
     }
 }
